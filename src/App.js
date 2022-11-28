fa57a264a4dc7e3137f8bab8c5fdb447149b5ef3
ElliotHarrison <ellioth1029@gmail.com>
2022-11-28 12:51:24 -0800

    first commit establishing a working react app without CRA

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const App = () => {
+  
+  return (
+    <div>
+      This is a React.js project built from scratch and without use of Create-React-App. Please check back soon for further updates!
+      
+    </div>
+  );
+};
+
+export default App;
